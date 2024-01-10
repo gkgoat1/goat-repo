923dcffbd0f78925fd66a876afe810139a61b9fd
Graham Kelly <gkgoat6700@gmail.com>
2024-01-10 18:01:42 -0500

    make subprojects work

---
+++ thingamajiggy/src/main/java/at/gkgo/thing/ThingamajiggyDataGenerator.java
@@ -0,0 +1,11 @@
+package at.gkgo.thing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThingamajiggyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
